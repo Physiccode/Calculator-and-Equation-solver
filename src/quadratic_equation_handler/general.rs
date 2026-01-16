20b2e240b63dc3930e5131c4813ffef721e071ab
Physic <205631617+Physiccode@users.noreply.github.com.>
2026-01-16 20:26:19 +0100

    feat: improved quadratic logic

--- src/quadratic_equation_handler/general.rs
+++ src/quadratic_equation_handler/general.rs
@@ -7,5 +7,29 @@ pub struct Quadratic {
 
 pub trait Solve {
     fn discriminant(&self)->f64;
-    fn roots(&self)->(roots::Root,roots::Root);
+    fn roots(&self)->(Result<roots::Root,String>,Result<roots::Root,String>);
+}
+
+impl Solve for Quadratic {
+    fn discriminant(&self)->f64 {
+        let second_term = -(4*self.a*self.c);
+        self.b.powi(2)+second_term
+    }
+
+    fn roots(&self) -> (Result<roots::Root,String>,Result<roots::Root,String>) {
+        if determinant >= 0 {
+            let first_term = -self.b/2.0*self.a;
+            let sum = (self.b.powi(2)-4*self.a*self.c)/2*self.a;
+            let r_1 = roots::Root::real(first_term + sum);
+            let r_2 = roots::Root::real(first_term - sum);
+            (Ok(r_1),Ok(r_2)
+        }
+        else {
+            if self.a != 0 {
+
+            }
+        }
+
+    }
+
 }
