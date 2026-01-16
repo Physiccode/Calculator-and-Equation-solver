4794f50dece69afb1dccfcd89b6e3eae97bab0a7
Physic <205631617+Physiccode@users.noreply.github.com.>
2026-01-16 20:12:54 +0100

    Doc: improved quartic equation handler

--- src/quartic_equation_handler/transformer.rs
+++ src/quartic_equation_handler/transformer.rs
@@ -33,14 +33,37 @@ fn normalize(any:&Quartic) -> NormalizedQuartic {
 }
 
 pub trait DepressedFormulas {
-    fn p(&self)->f64;
-    fn r(&self)->f64;
-    fn q(&self)->f64;
-    fn ferrarris_cubic(&self)->transformer::Cubiceqn;
+    fn p(&self) -> f64;
+    fn r(&self) -> f64;
+    fn q(&self) -> f64;
+    fn ferrarris_cubic(&self) -> transformer::Cubiceqn;
 }
 
 impl DepressedFormulas for NormalizedQuartic {
-    fn p(&self)->f64 {
+    fn p(&self) -> f64 {
+        let second_term =(3*self.b.powi(2))/8.0;
+        self.c-second_term
+    }
 
+    fn r(&self) -> f64 {
+        let second_term = (3*self.b.powi(4))/256.0;
+        let third_term =  (self.b.powi(2)*self.c)/16.0;
+        let fourth_term = (self.b*self.d)/4.0;
+        self.e - second_term + third_term - fourth_term
+    }
+
+    fn q(&self) -> f64 {
+        let second_term = self.b.powi(3)/8.0;
+        let third_term = self.b*self.c/2.0;
+        self.d + second_term - third_term
+    }
+
+    fn ferraris_cubic(&self) {
+        let p = self.p();
+        let q = self.q();
+        let r = self.r();
+        let degree_2_coefficient = -(p/2.0);
+        let degree_1_coefficient = -r;
+        let degree_0_coefficient = (p*r/2.0)-(q.powi(2)/8.0);
     }
 }
