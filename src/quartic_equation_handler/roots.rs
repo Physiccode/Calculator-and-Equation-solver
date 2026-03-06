a52a73261e013671396cd09cfd56827d88792c16
Physic <205631617+Physiccode@users.noreply.github.com.>
2026-03-06 19:49:47 +0100

    improved quartic

--- src/quartic_equation_handler/roots.rs
+++ src/quartic_equation_handler/roots.rs
@@ -1,4 +1,14 @@
 use crate::cubic_equation_handler::transformer;
-use crate::quartic_equation_handler;
+use crate::quadratic_equation_handler::general;
+use crate::quartic_equation_handler; //transformer //get the quadratic equation structure
 
-fn get_real_root(any: &cubicequation_handler);
+/*Assuming you have a real solution formed by the ferrari's cubic,say,z.To solve it and get
+the roots in y and in x,the code bellow  will show how */
+
+pub fn get_u(z: f64, p: f64) -> f64 {
+    first_term = (2 * z);
+    return (first_term - p).sqrt();
+}
+
+//get_v depends on the value of u,which is obtained after running get_u()
+pub fn get_v(q: f64, u: f64) -> f64 {}
